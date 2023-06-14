0d437d55353c502402e85def416e4c2342059fc7
Nearh777 <nearh777@gmail.com>
2023-06-14 22:14:30 +0300

    start

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Title} from 'HomePage.styled.jsx';
+
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Title >Trending movies of the week</Title>
+      <MovieList />
+    </Box>
+  )
+}
+
+export default HomePage
